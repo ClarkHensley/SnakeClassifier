ad9277c6f7c413e0d76c667ed0d7c7cbf5c76f5a
Clark Hensley <clarkhensley@protonmail.com>
2021-12-07 11:16:05 -0600

    Where'd I get that?

--- import_tensorflow.py
+++ import_tensorflow.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env Python3
 
+# From: https://weepingfish.github.io/2020/07/22/0722-suppress-tensorflow-warnings/
+
 def import_tensorflow():
     import os
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
